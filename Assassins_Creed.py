78074a81cd7067f9a313b1c688863bb9f2456e6d
Legend <legendartis@gmail.com>
2023-08-21 18:32:11 -0400

    Add files via upload

--- Assassins_Creed.py
+++ Assassins_Creed.py
@@ -1,98 +1,14 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import numpy as np
 
-###################################
-# Dataframes for each Excel sheet #
-###################################
+# 'r' in front converts normal string into a raw string
+df = pd.read_csv(r"C:\Users\cvhs3\Desktop\Python_files\Assassin's Creed Project\AC_stats\AC_vgsales.csv")
+print(df[:5])
 
-# importing Excel data
-gameRankings = pd.read_excel('AC_stats.xlsx','GameRankings')
-metacritic = pd.read_excel('AC_stats.xlsx','Metacritic')
-acInfo = pd.read_excel('AC_stats.xlsx','Jogos por ano e plataforma')
+# dropping all rows with empty columns ranging from 'Critic_Score' to 'Global_Score'
+df2 = df.dropna(subset=['Critic_Score', 'User_Score', 'NA_Sales', 'PAL_Sales', 'JP_Sales', 'Other_Sales',
+                        'Global_Sales'], how='all')
+# replacing 'Global_Sales' values as sum of all sales
+df2['Global_Sales'] = df2.loc[:, 'NA_Sales':'Other_Sales'].sum(axis=1)
 
-############################
-# GameRanking correlations #
-############################
-
-gameRankings.corr(numeric_only=True)
-sns.heatmap(gameRankings.corr(numeric_only=True), annot = True)
-plt.rcParams['figure.figsize'] = (25,7)
-plt.gcf().subplots_adjust(left=0.145)
-plt.gcf().subplots_adjust(bottom=0.183)
-plt.show()
-
-##########################
-# GameRankings bar chart #
-##########################
-
-plt.figure(figsize=(8,5))
-plt.barh(gameRankings['Jogo'], gameRankings['Avg_Score'], color='orange')
-plt.xlabel('Review Score')
-plt.title("All GameRanking Scores of Assassin's Creed Games")
-plt.grid(True)
-plt.gcf().subplots_adjust(left=0.383)
-plt.show()
-
-########################
-# Metacritic bar chart #
-########################
-
-plt.figure(figsize=(8,5))
-plt.barh(metacritic['Jogo'], metacritic['Avg_Score'], color='orange')
-plt.xlabel('Review Score')
-plt.title("All Metacritic Scores of Assassin's Creed Games")
-plt.grid(True)
-plt.gcf().subplots_adjust(left=0.385)
-plt.show()
-
-####################################################
-# Grouped bar graph of GameRankings and Metacritic #
-####################################################
-
-# merging GameRankings and Metacritic dataframes together
-df = pd.concat([gameRankings, metacritic], axis=1, join='inner')
-
-# finding duplicate column names and adding a .# to the end of them
-cols = pd.Series(df.columns)
-
-for dup in cols[cols.duplicated()].unique():
-	cols[cols[cols == dup].index.values.tolist()] = [dup + '.' 
-	+ str(i) if i != 0 else dup for i in range(sum(cols == dup))]
-df.columns = cols
-
-# Renamed 'Avg_Score' and 'Avg_Score.1' to represent GamingRankings & Metacritic
-df.rename(columns={'Avg_Score': 'GameRankings', 'Avg_Score.1': 'Metacritic'}, inplace=True)
-
-# dropping unneeded columns
-df.drop(df.iloc[:, 1:10], inplace=True, axis=1)
-df.drop(df.iloc[:, 2:14], inplace=True, axis=1)
-
-# setting index to 'Jogo'
-df.set_index('Jogo', inplace=True)
-
-print(df)
-
-# plotting grouped bar graph
-df.plot(kind='barh', figsize=(8,6))
-plt.xlabel("Review Score")
-plt.ylabel("")
-plt.title('Average Review Scores')
-plt.legend(loc='upper right')
-plt.grid(True)
-plt.gcf().subplots_adjust(left=0.337)
-plt.show()
-
-###################################################
-# Correlation between GameRankings and Metacritic #
-###################################################
-
-gameRankings['Avg_Score'].corr(metacritic['Avg_Score'])
-sns.heatmap(gameRankings.corr(numeric_only=True), annot = True)
-plt.rcParams['figure.figsize'] = (10,6)
-plt.show()
-
-######################################################
-# Combining all previous graphs into one single page #
-######################################################
-
+print(df2)
